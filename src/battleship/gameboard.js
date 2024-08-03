6acbeae5321dd1430438ce0549cbbb5f2b24f48e
lloydeebill <lloydbill.lariosa@gmail.com>
2024-08-03 12:05:37 +0800

    from chosenIndex === centerIndex , used this to make previewBoard centered for odd sized ship

--- src/battleship/gameboard.js
+++ src/battleship/gameboard.js
@@ -19,24 +19,38 @@ class Gameboard {
     cells.forEach((cell, index) => {
       cell.addEventListener("mouseover", () => {
         const ship = getShipChosen();
-        const startIndex = index;
 
-        this.previewBoardShip(startIndex, ship);
+        // centerIndex === chosenIndex
+        const centerIndex = index;
+
+        this.previewBoardShip(centerIndex, ship);
       });
     });
 
     return board;
   }
 
-  previewBoardShip(index, ship) {
-    console.log(index);
+  previewBoardShip(centerIndex, ship) {
+    console.log(centerIndex);
     console.log(ship);
 
     const cells = document.querySelectorAll(`.${this.user}-board .cell`);
     cells.forEach((cell) => cell.classList.remove("ship-preview"));
-    for (let i = index; i < index + ship.size; i++) {
-      if (cells[i]) {
-        cells[i].classList.add("ship-preview");
+
+    if (ship.size % 2 !== 0) {
+      const halfSize = Math.floor(ship.size / 2);
+      const startIndex = centerIndex - halfSize;
+      const endIndex = centerIndex + halfSize;
+      for (let i = startIndex; i <= endIndex; i++) {
+        if (cells[i]) {
+          cells[i].classList.add("ship-preview");
+        }
+      }
+    } else {
+      for (let i = centerIndex; i < centerIndex + ship.size; i++) {
+        if (cells[i]) {
+          cells[i].classList.add("ship-preview");
+        }
       }
     }
   }
